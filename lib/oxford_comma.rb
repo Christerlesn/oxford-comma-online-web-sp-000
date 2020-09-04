6e893c6bc8e622ec4d92304ffb851997880093bf
Christerle Nounoute <christerlesn@gmail.com>
2020-09-04 18:03:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
 def oxford_comma(array)
 if array.length == 1
-  return array.join
+  return "#{array[0]}"
 elsif array.length == 2
-  return array.split("and")
-elsif array.length == 3
-  return array.split(",") && array.last << ("and")
-else array.length > 3
-  return
+  return array.join(" and ")
+elsif array.length >= 3
+  array[-1] = "and #{array[-1]}"
+  return array.join(", ")
 end
 end
